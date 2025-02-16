e8dcd50e1d93384de24c8305e1817b6f5b5eeddb
ThuRa Lynn Htun <lynnhtunthura8@gmail.com>
2025-02-16 16:57:00 +0630

    Finish styling and structuring Products.jsx

--- src/Products/Products.jsx
+++ src/Products/Products.jsx
@@ -29,7 +29,7 @@ const Products = () => {
               </div>
 
               <div className="bag">
-                <BsFillBagHeartFill />
+                <BsFillBagHeartFill className="bag-icon" />
               </div>
             </section>
           </div>
